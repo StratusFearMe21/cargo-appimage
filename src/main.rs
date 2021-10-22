47e87d75149cf686e7555aff51f0d327c83fc313
Jim Hessin <jhessin@gmail.com>
2021-10-22 16:26:57 -0600

    Feature: Add a blank icon.img if none is specified.

--- src/main.rs
+++ src/main.rs
@@ -3,29 +3,34 @@ use std::process::Command;
 use anyhow::Context;
 
 fn main() -> anyhow::Result<()> {
-    match Command::new("cargo").arg("build").arg("--release").status() {
-        Ok(_) => {}
-        Err(_) => panic!("Failed to build package"),
+  match Command::new("cargo").arg("build").arg("--release").status() {
+    Ok(_) => {}
+    Err(_) => panic!("Failed to build package"),
+  }
+  match std::path::Path::new("./icon.png").exists() {
+    true => {}
+    false => {
+      Command::new("touch")
+        .arg("icon.png")
+        .status()
+        .context("Failed to generate icon.png")?;
     }
-    match std::path::Path::new("./icon.png").exists() {
-        true => {}
-        false => panic!("Cannot find icon"),
-    }
-    let meta = cargo_toml::Manifest::from_path("./Cargo.toml")
-        .context("Cannot find Cargo.toml")?
-        .package
-        .unwrap();
-    std::fs::create_dir("target/cargo-appimage.AppDir").unwrap_or(());
-    std::fs::create_dir("target/cargo-appimage.AppDir/usr").unwrap_or(());
-    std::fs::create_dir("target/cargo-appimage.AppDir/usr/bin").unwrap_or(());
-    std::fs::copy(
-        format!("target/release/{}", meta.name),
-        "target/cargo-appimage.AppDir/usr/bin/bin",
-    )
-    .context("Cannot find binary file")?;
-    std::fs::copy("./icon.png", "target/cargo-appimage.AppDir/icon.png")
-        .context("Cannot find icon.png")?;
-    std::fs::write(
+  }
+  let meta = cargo_toml::Manifest::from_path("./Cargo.toml")
+    .context("Cannot find Cargo.toml")?
+    .package
+    .unwrap();
+  std::fs::create_dir("target/cargo-appimage.AppDir").unwrap_or(());
+  std::fs::create_dir("target/cargo-appimage.AppDir/usr").unwrap_or(());
+  std::fs::create_dir("target/cargo-appimage.AppDir/usr/bin").unwrap_or(());
+  std::fs::copy(
+    format!("target/release/{}", meta.name),
+    "target/cargo-appimage.AppDir/usr/bin/bin",
+  )
+  .context("Cannot find binary file")?;
+  std::fs::copy("./icon.png", "target/cargo-appimage.AppDir/icon.png")
+    .context("Cannot find icon.png")?;
+  std::fs::write(
         "target/cargo-appimage.AppDir/cargo-appimage.desktop",
         format!(
             "[Desktop Entry]\nName={}\nExec=bin\nIcon=icon\nType=Application\nCategories=Utility;",
