e1e669eea515f8c9e10a74049c0a35b949675387
Isaac Mills <rooster0055@protonmail.com>
2021-10-26 11:41:49 -0400

    Runner changes working dir to exe

--- src/apprun.rs
+++ src/apprun.rs
@@ -1,20 +1,26 @@
-fn main() {
-    let here_dir = std::path::PathBuf::from(std::env::current_exe().unwrap());
-    std::env::set_var("LD_LIBRARY_PATH", format!("{}/usr/lib/:{}/usr/lib/i386-linux-gnu/:{}/usr/lib/x86_64-linux-gnu/:{}/usr/lib32/:{}/usr/lib64/:{}/lib/:{}/lib/i386-linux-gnu/:{}/lib/x86_64-linux-gnu/:{}/lib32/:{}/lib64/:{}", here_dir.parent().unwrap().display(), here_dir.parent().unwrap().display(), here_dir.parent().unwrap().display(), here_dir.parent().unwrap().display(), here_dir.parent().unwrap().display(), here_dir.parent().unwrap().display(), here_dir.parent().unwrap().display(), here_dir.parent().unwrap().display(), here_dir.parent().unwrap().display(), here_dir.parent().unwrap().display(), std::env::var("LD_LIBRARY_PATH").unwrap_or(String::new())));
+use anyhow::Context;
+
+fn main() -> anyhow::Result<()> {
+    let here_dir = std::path::PathBuf::from(std::env::current_exe()?);
+    let parent = here_dir
+        .parent()
+        .with_context(|| format!("{} has no parent directory", &here_dir.display()))?;
+    std::env::set_current_dir(&parent)?;
+    std::env::set_var(
+        "LD_LIBRARY_PATH",
+        format!("{}/usr/lib/:{}/usr/lib/i386-linux-gnu/:{}/usr/lib/x86_64-linux-gnu/:{}/usr/lib32/:{}/usr/lib64/:{}/lib/:{}/lib/i386-linux-gnu/:{}/lib/x86_64-linux-gnu/:{}/lib32/:{}/lib64/{}", parent.display(), parent.display(), parent.display(), parent.display(), parent.display(), parent.display(), parent.display(), parent.display(), parent.display(), parent.display(), if let Ok(ldlibpath) = std::env::var("LD_LIBRARY_PATH") { ":".to_string() + &ldlibpath } else { String::new() }),
+    );
     std::env::set_var(
         "XDG_DATA_DIRS",
         format!(
             "XDG_DATA_DIRS={}:{}",
-            here_dir.parent().unwrap().join("usr/share").display(),
+            parent.join("usr/share").display(),
             std::env::var("XDG_DATA_DIRS").unwrap_or(String::new())
         ),
     );
-    let err = exec::execvp(
-        here_dir.parent().unwrap().join("usr/bin/bin"),
-        std::env::args(),
-    );
+    let err = exec::execvp(parent.join("usr/bin/bin"), std::env::args());
     eprintln!("Error: {}", err);
-    std::process::exit(1);
+    Ok(())
 }
 
 // .env(
