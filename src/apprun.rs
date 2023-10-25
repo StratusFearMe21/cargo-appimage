a9f21a4064dcba500ffcd25b5d7ea634f6130e8a
fililip <tajemniczymeusik@gmail.com>
2023-10-25 11:09:59 +0200

    code cleanup as per clippy recommendations

--- src/apprun.rs
+++ src/apprun.rs
@@ -2,11 +2,11 @@ use anyhow::Context;
 use std::fs;
 
 fn main() -> anyhow::Result<()> {
-    let here_dir = std::path::PathBuf::from(std::env::current_exe()?);
+    let here_dir = std::env::current_exe()?;
     let parent = here_dir
         .parent()
         .with_context(|| format!("{} has no parent directory", &here_dir.display()))?;
-    std::env::set_current_dir(&parent)?;
+    std::env::set_current_dir(parent)?;
     std::env::set_var(
         "LD_LIBRARY_PATH",
         format!("{}/usr/lib/:{}/usr/lib/i386-linux-gnu/:{}/usr/lib/x86_64-linux-gnu/:{}/usr/lib32/:{}/usr/lib64/:{}/lib/:{}/lib/i386-linux-gnu/:{}/lib/x86_64-linux-gnu/:{}/lib32/:{}/lib64/{}", parent.display(), parent.display(), parent.display(), parent.display(), parent.display(), parent.display(), parent.display(), parent.display(), parent.display(), parent.display(), if let Ok(ldlibpath) = std::env::var("LD_LIBRARY_PATH") { ":".to_string() + &ldlibpath } else { String::new() }),
